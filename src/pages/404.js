05557fda1352c4e407f819f69d8483cdf99ab4e1
Milan Rusimov <rusmil89@gmail.com>
2023-01-30 11:18:06 +0100

    Revert "!X - Init commit with theFront template"
    This reverts commit 56a98e6b8a4455df4f29c4b2ae85d096fb9caf79.

--- src/pages/404.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import NotFoundCover from 'views/NotFoundCover';
-
-const FourOFourPage = () => {
-  return <NotFoundCover />;
-};
-
-export default FourOFourPage;
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import IndexView from 'views/IndexView';
-
-const IndexPage = () => {
-  return <IndexView />;
-};
-
-export default IndexPage;
