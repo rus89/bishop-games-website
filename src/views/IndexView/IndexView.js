e673ec047a18cfbddcb336c32c6dbe6b1a59412f
Milan Rusimov <rusmil89@gmail.com>
2023-02-01 10:43:57 +0100

    !X - Replaced Services component
    Why:
    
    It's more beautiful

--- src/views/IndexView/IndexView.js
+++ src/views/IndexView/IndexView.js
@@ -15,6 +15,7 @@ const IndexView = () => {
 
   return (
     <Main>
+      {/* Hero */}
       <Box
         position={'relative'}
         sx={{
@@ -44,9 +45,30 @@ const IndexView = () => {
           ></path>
         </Box>
       </Box>
+
+      {/* Services */}
       <Container paddingTop={'0 !important'}>
         <Services />
       </Container>
+      <Box
+        component={'svg'}
+        preserveAspectRatio="none"
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        viewBox="0 0 1920 100.1"
+        sx={{
+          width: '100%',
+          marginBottom: theme.spacing(-1),
+        }}
+      >
+        <path
+          fill={theme.palette.alternate.main}
+          d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+        ></path>
+      </Box>
+
+      {/* workflow */}
       <Container maxWidth={1} paddingTop={'0 !important'}
         sx={{
           backgroundColor: theme.palette.alternate.main,
@@ -55,80 +77,25 @@ const IndexView = () => {
         <Workflow />
       </Container>
       <Box
-        position={'relative'}
+        component={'svg'}
+        preserveAspectRatio="none"
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        viewBox="0 0 1920 100.1"
         sx={{
-          backgroundColor: theme.palette.alternate.main,
+          width: '100%',
+          marginBottom: theme.spacing(-1),
         }}
       >
-        <Box>
-          {/* <Container>
-            <Contact />
-          </Container> */}
-          <Container paddingTop={'0 !important'}>
-            {/* <About /> */}
-          </Container>
-        </Box>
-        <Box
-          component={'svg'}
-          preserveAspectRatio="none"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 1920 100.1"
-          sx={{
-            width: '100%',
-            marginBottom: theme.spacing(-1),
-          }}
-        >
-          <path
-            fill={theme.palette.background.paper}
-            d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-          ></path>
-        </Box>
+        <path
+          fill={theme.palette.background.paper}
+          d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+        ></path>
       </Box>
-      {/* <Container paddingTop={'0 !important'}>
-        <Users />
-      </Container> */}
-      <Box bgcolor={'alternate.main'}>
-        <Container>
-          {/* <Reviews /> */}
-        </Container>
-      </Box>
-      {/* <Container>
-        <Features />
-      </Container> */}
-      {/* <Box
-        position={'relative'}
-        sx={{
-          backgroundColor: theme.palette.alternate.main,
-        }}
-      >
-        <Box>
-          <Container>
-            <Pricings />
-          </Container>
-          <Container paddingTop={'0 !important'}>
-            <Faq />
-          </Container>
-        </Box>
-        <Box
-          component={'svg'}
-          preserveAspectRatio="none"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 1920 100.1"
-          sx={{
-            width: '100%',
-            marginBottom: theme.spacing(-1),
-          }}
-        >
-          <path
-            fill={theme.palette.background.paper}
-            d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-          ></path>
-        </Box>
-      </Box> */}
+
+      {/* Our Team */}
+      
     </Main>
   );
 };
--- src/views/IndexView/components/Services/Services.js
+++ src/views/IndexView/components/Services/Services.js
@@ -1,31 +1,196 @@
 import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-
+import Card from '@mui/material/Card';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 import Container from 'components/Container';
-import { FeatureList, MasonryCards } from './components';
+
+const mock = [
+  {
+    title: 'Full Game Development',
+    subtitle:
+      'You have an idea, we create a real game',
+    icon: (
+      <svg
+        height={24}
+        width={24}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: 'Game Art',
+    subtitle:
+      'You have a great code, we provide a great art',
+    icon: (
+      <svg
+        height={24}
+        width={24}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: 'Game programming',
+    subtitle:
+      'You have a great art, we make it alive',
+    icon: (
+      <svg
+        height={24}
+        width={24}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: 'Animation',
+    subtitle:
+      'You have static images, we make them dancing',
+    icon: (
+      <svg
+        height={24}
+        width={24}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M13 10V3L4 14h7v7l9-11h-7z"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: 'Consulting',
+    subtitle: 'You need someone to keep your back, we are here',
+    icon: (
+      <svg
+        height={24}
+        width={24}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: 'Live Ops',
+    subtitle:
+      'You made unexpectedly successful game, we support new content creation',
+    icon: (
+      <svg
+        height={24}
+        width={24}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+        />
+      </svg>
+    ),
+  },
+];
 
 const Services = () => {
+  const theme = useTheme();
   return (
     <Container>
-      <Grid container spacing={4}>
-        <Grid
-          item
-          xs={12}
-          md={6}
+      <Box marginBottom={8}>
+        <Typography
+          variant="h2"
+          color="text.primary"
+          align={'center'}
+          gutterBottom
           sx={{
-            backgroundColor: 'transparent',
-            backgroundImage:
-              'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 1600 900\'%3E%3Cdefs%3E%3ClinearGradient id=\'a\' x1=\'0\' x2=\'0\' y1=\'1\' y2=\'0\' gradientTransform=\'rotate(0,0.5,0.5)\'%3E%3Cstop offset=\'0\' stop-color=\'%230FF\'/%3E%3Cstop offset=\'1\' stop-color=\'%23CF6\'/%3E%3C/linearGradient%3E%3ClinearGradient id=\'b\' x1=\'0\' x2=\'0\' y1=\'0\' y2=\'1\' gradientTransform=\'rotate(0,0.5,0.5)\'%3E%3Cstop offset=\'0\' stop-color=\'%23F00\'/%3E%3Cstop offset=\'1\' stop-color=\'%23FC0\'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill=\'%23FFF\' fill-opacity=\'0\' stroke-miterlimit=\'10\'%3E%3Cg stroke=\'url(%23a)\' stroke-width=\'21.12\'%3E%3Cpath d=\'M1409 581 1450.35 511 1490 581z\'/%3E%3Ccircle stroke-width=\'7.040000000000001\' transform=\'\' cx=\'500\' cy=\'100\' r=\'40\'/%3E%3Cpath transform=\'\' d=\'M400.86 735.5h-83.73c0-23.12 18.74-41.87 41.87-41.87S400.86 712.38 400.86 735.5z\'/%3E%3C/g%3E%3Cg stroke=\'url(%23b)\' stroke-width=\'6.4\'%3E%3Cpath transform=\'\' d=\'M149.8 345.2 118.4 389.8 149.8 434.4 181.2 389.8z\'/%3E%3Crect stroke-width=\'14.080000000000002\' transform=\'\' x=\'1039\' y=\'709\' width=\'100\' height=\'100\'/%3E%3Cpath transform=\'\' d=\'M1426.8 132.4 1405.7 168.8 1363.7 168.8 1342.7 132.4 1363.7 96 1405.7 96z\'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")',
-            backgroundSize: '100% 100%',
-            backgroundRepeat: 'no-repeat',
+            fontWeight: 700,
           }}
         >
-          <MasonryCards />
-        </Grid>
-        <Grid item container alignItems={'center'} xs={12} md={6}>
-          <FeatureList />
-        </Grid>
-      </Grid>
+          Our Services
+        </Typography>
+      </Box>
+      <Grid container spacing={4}>
+        {mock.map((item, i) => (
+          <Grid item xs={12} sm={6} md={4} key={i}>
+            <Box
+              component={Card}
+              padding={4}
+              borderRadius={2}
+              width={1}
+              height={1}
+              data-aos={'fade-up'}
+              data-aos-delay={i * 100}
+            >
+              <Box display={'flex'} flexDirection={'column'}>
+                <Box
+                  component={Avatar}
+                  width={50}
+                  height={50}
+                  marginBottom={2}
+                  bgcolor={theme.palette.primary.main}
+                  color={theme.palette.background.paper}
+                >
+                  {item.icon}
+                </Box>
+                <Typography variant={'h6'} gutterBottom sx={{ fontWeight: 500 }}>
+                  {item.title}
+                </Typography>
+                <Typography color="text.secondary">{item.subtitle}</Typography>
+              </Box>
+            </Box>
+          </Grid>
+        ))
+        }
+      </Grid >
     </Container>
   );
 };
