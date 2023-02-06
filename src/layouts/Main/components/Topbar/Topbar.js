cb299592648da27b8cf1669a2557b9da8ef03e97
Milan Rusimov <rusmil89@gmail.com>
2023-02-06 19:57:55 +0100

    !X - Added StaticImage component to topbar to render logo
    Why:
    
    Optimizing performances

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { alpha, useTheme } from '@mui/material/styles';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import { StaticImage } from 'gatsby-plugin-image';
 import { NavItem } from './components';
 import { graphql, useStaticQuery } from 'gatsby';
 
@@ -38,11 +38,13 @@ query {
         title="Bishop Games"
         width={{ xs: 100, md: 120 }}
       >
-        <Box
-          component={'img'}
-          src={'../../../../../images/logo/logo_v2.png'}
-          height={0.4}
-          width={0.4}
+        <StaticImage
+          src='../../../../images/logo/logo_v2.png'
+          alt='logo image'
+          style={{
+            height: '40%',
+            width: '40%',
+          }}
         />
       </Box>
       {/* //TODO: ovo treba napraviti dinamicki mozda da se itemi i id povlace iz nekog fajla kako se kod ne bi menjao svaki put kada se doda ili oduzme neka komponenta */}
