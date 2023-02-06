26b11be347eea51f99710cf3300a081c7db47c68
Milan Rusimov <rusmil89@gmail.com>
2023-02-06 11:48:22 +0100

    !X - Changed font to 'Ubuntu'
    Why:
    
    Better looking

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -55,8 +55,9 @@ export default function TopLayout(props) {
           property="og:url"
           content="https://www.bishop.games"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
       </Helmet>
       <StrictMode>
         <Page>
