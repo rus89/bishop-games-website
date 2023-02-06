8cdddc091dce638474aa3a1ef8c087454164235d
Milan Rusimov <rusmil89@gmail.com>
2023-02-06 18:23:40 +0100

    !X - Added initial version of FAQ component
    Why:
    
    To provide more info to our potential clients

--- src/views/IndexView/components/Faq/Faq.js
+++ src/views/IndexView/components/Faq/Faq.js
@@ -9,6 +9,49 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Container from 'components/Container';
 
+const faqs = [
+  {
+    question: 'What services does your Unity game development studio offer?',
+    answer: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+  },
+  {
+    question: 'Can you provide examples of previous projects you have worked on?',
+    subtitle: 'Yes, we have worked on a variety of projects ranging from small hyper-casual games to larger, more complex games. Please feel free to take a look at our portfolio or references from our previous clients.'
+  },
+  {
+    question: 'What is your process for game development projects?',
+    subtitle: 'Our process typically involves an initial consultation, project planning, development, testing and deployment. Our team works closely with clients to ensure that the project is delivered on time and within budget.'
+  },
+  {
+    question: 'How do you manage communication and collaboration with clients?',
+    subtitle: 'We use project management tools as Notion and regular check-ins to ensure effective communication and collaboration with clients. Our team is also available for virtual meetings and updates as needed.'
+  },
+  {
+    question: 'Can you handle projects with tight deadlines?',
+    subtitle: 'Yes, we have a track record of delivering projects on time and can handle tight deadlines by utilizing our team of experienced game developers.'
+  },
+  {
+    question: 'What is your pricing structure for game development projects?',
+    subtitle: 'Our pricing structure is based on the scope and complexity of the project. We offer competitive rates and are flexible with our pricing to meet the needs of our clients. Our mission is to create a great value.'
+  },
+  {
+    question: 'How do you ensure the quality of your work?',
+    subtitle: 'We have a rigorous quality control process in place, which includes regular testing and quality assurance checks throughout the development process. Our team is also dedicated to delivering high-quality work and ensuring client satisfaction.'
+  },
+  {
+    question: 'Can you provide after-sales support for the games you develop?',
+    subtitle: 'Yes, we offer after-sales support and maintenance services for the games we develop. Our team is available for any updates or modifications that may be required post-deployment.'
+  },
+  {
+    question: 'How do you handle intellectual property and confidentiality?',
+    subtitle: 'We take intellectual property and confidentiality very seriously and have strict policies in place to ensure that client information is kept confidential.'
+  },
+  {
+    question: 'Can you provide references or case studies from previous clients?',
+    subtitle: 'Yes, we are happy to provide references or case studies from previous clients upon request. Our goal is to build long-term relationships with our clients and deliver high-quality work that exceeds their expectations.'
+  },
+];
+
 const FaqGroupItem = ({ title, items }) => {
   const theme = useTheme();
   return (
@@ -38,10 +81,10 @@ const FaqGroupItem = ({ title, items }) => {
               aria-controls="panel1a-content"
               id={`panel1a-header--${i}`}
             >
-              <Typography fontWeight={600}>{item.title}</Typography>
+              <Typography fontWeight={600}>{item.question}</Typography>
             </Box>
             <AccordionDetails>
-              <Typography color="text.secondary">{item.subtitle}</Typography>
+              <Typography color="text.secondary">{item.answer}</Typography>
             </AccordionDetails>
           </Box>
         ))}
@@ -57,6 +100,7 @@ FaqGroupItem.propTypes = {
 
 const Faq = () => {
   const theme = useTheme();
+
 
   return (
     <Container data-aos={'fade-up'}>
@@ -95,33 +139,7 @@ const Faq = () => {
           data-aos-delay={100}>
           <FaqGroupItem
             title={'Basics'}
-            items={[
-              {
-                title: 'Can I purchase a gift certificate?',
-                subtitle:
-                  'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-              },
-              {
-                title: 'What is your return policy?',
-                subtitle:
-                  'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-              },
-              {
-                title: 'Do you sell gift cards?',
-                subtitle:
-                  'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-              },
-              {
-                title: 'Can I change plans later on?',
-                subtitle:
-                  'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-              },
-              {
-                title: 'Is this a subscription service?',
-                subtitle:
-                  'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-              },
-            ]}
+            items={faqs}
           />
         </Box>
       </Box>
