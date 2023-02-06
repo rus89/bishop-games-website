35cfde795cd45524bc2e8aea897eea351f1b154d
Milan Rusimov <rusmil89@gmail.com>
2023-02-06 18:40:44 +0100

    !X - Added favicon
    Why:
    
    Better looking

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -13,15 +13,11 @@ export default function TopLayout(props) {
     <React.Fragment>
       <Helmet>
         <meta charSet="utf-8" />
-        {/* <link
-          rel="shortcut icon"
-          href="https://assets.maccarianagency.com/favicons/thefront/favicon.ico"
-        /> */}
         <link rel="shortcut icon" href="/favicon.ico" />
-        {/* <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png" />
-        <link rel="manifest" href="../site.webmanifest" /> */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
