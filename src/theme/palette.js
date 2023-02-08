f48a8bcab7d7e3468de61afb0b663cf8edc4c267
Milan Rusimov <rusmil89@gmail.com>
2023-02-08 17:05:34 +0100

    !X - Completed color palette
    Why:
    
    Must have

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -3,15 +3,15 @@ export const light = {
   cardShadow: 'rgba(23, 70, 161, .11)',
   divider: 'rgba(0, 0, 0, 0.12)',
   primary: {
+    light: '#f4814e',
     main: '#f16a22',
-    light: '#467de3',
-    dark: '#2f6ad9',
+    dark: '#dd5317',
     contrastText: '#fff',
   },
   secondary: {
-    light: '#ffb74d',
-    main: '#f9b934',
-    dark: '#FF9800',
+    light: '#c93f3c',
+    main: '#c52127',
+    dark: '#b70916',
     contrastText: 'rgba(0, 0, 0, 0.87)',
   },
   alternate: {
@@ -20,13 +20,11 @@ export const light = {
   },
   background: {
     default: '#ffffff',
-    paper: '#ffffff',
     level1: '#444444',
     level2: '#f3f3f3',
   },
   text: {
     primary: '#3c3c3c',
     secondary: '#444444',
-    ternary: '#f1814e'
   },
 };
