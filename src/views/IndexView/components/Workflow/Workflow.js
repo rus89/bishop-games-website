daebe6b8828284a860462ced813298bd984cbd5d
Milan Rusimov <rusmil89@gmail.com>
2023-02-20 11:39:08 +0100

    !X - Added <li> under <ul> in workflow component
    Why:
    
    Accessiblity improvements

--- src/views/IndexView/components/Workflow/Workflow.js
+++ src/views/IndexView/components/Workflow/Workflow.js
@@ -203,42 +203,37 @@ const Workflow = () => {
             <Box sx={{ mt: 2, mb: 1, p: 2 }} justifyContent={'center'} align={'center'}>
               <Grid container spacing={1} component={List}>
                 {steps[activeStep].description.split('-').slice(1).map((item, i) => (
-                  <Grid item xs={12} sm={6} key={i}>
+                  <Grid item xs={12} sm={6} key={i}
+                    component={ListItem}
+                    width={'auto'}
+                    padding={0}>
                     <Box
-                      component={ListItem}
-                      disableGutters
-                      width={'auto'}
-                      padding={0}
-                      key={i}
+                      component={ListItemAvatar}
+                      minWidth={'auto !important'}
+                      marginRight={2}
                     >
                       <Box
-                        component={ListItemAvatar}
-                        minWidth={'auto !important'}
-                        marginRight={2}
+                        component={Avatar}
+                        bgcolor={theme.palette.warning.light}
+                        width={20}
+                        height={20}
                       >
-                        <Box
-                          component={Avatar}
-                          bgcolor={theme.palette.warning.light}
-                          width={20}
-                          height={20}
+                        <svg
+                          width={12}
+                          height={12}
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
                         >
-                          <svg
-                            width={12}
-                            height={12}
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </Box>
+                          <path
+                            fillRule="evenodd"
+                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
                       </Box>
-                      <ListItemText primary={item} />
                     </Box>
+                    <ListItemText primary={item} />
                   </Grid>
                 ))}
               </Grid>
