cdc5fb73083af21de949925c8ca5ad2ec5d62225
Milan Rusimov <rusmil89@gmail.com>
2026-04-14 14:08:15 +0200

    chore: remove slick CSS imports and unused hero background image

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 
 import Page from '../../src/components/Page';
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import 'aos/dist/aos.css';
 
 export default function TopLayout(props) {
